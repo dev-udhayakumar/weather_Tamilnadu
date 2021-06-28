7c30f464a91a8089b89dc1072db5231d1a4a8620
udhay5021 <60465112+udhayasiva@users.noreply.github.com>
2021-06-28 21:35:50 +0530

    2

--- weather.js
+++ weather.js
@@ -1,5 +1,6 @@
 function weather(){
-    link='https://api.weatherapi.com/v1/current.json?key=7193a037355848eca6921203212806&q=Avanashi'
+    var place =  document.getElementById("place").value
+    link='https://api.weatherapi.com/v1/current.json?key=7193a037355848eca6921203212806&q='+place
     
     async function result(){
         var pr = await fetch(link)
@@ -11,12 +12,15 @@ function weather(){
         var condition = results.current.condition.text
         var humidity = results.current.humidity
         var wind_kph = results.current.wind_kph
+        var last_updated = results.current.last_updated
     
         document.getElementById("name").innerHTML = city +","+region;
-        document.getElementById("1").innerHTML = "Temperature: "+temp_f+"°F  | " + temp_c + "°C " ;
+        document.getElementById("1").innerHTML = "Temperature: "+temp_c+"°C  | " + temp_f + "°F " ;
         document.getElementById("2").innerHTML = "Condition: "+condition;
         document.getElementById("3").innerHTML = "Humidity : "+humidity +"%  | Wind : " + wind_kph +"kph" ;
-        console.log(results)
+        document.getElementById("uptime").innerHTML = "last updated on "+last_updated;
+        console.log()
+        
         
     }
     result()
