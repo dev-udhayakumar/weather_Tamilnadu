4a097ad45c6d6c61b70a0b2ee922faeeb0de0ba7
udhay5021 <60465112+udhayasiva@users.noreply.github.com>
2021-06-29 12:33:37 +0530

    3

--- weather.js
+++ weather.js
@@ -1,5 +1,44 @@
+function places(){
+    var input = document.getElementById("place").value
+    var areas = [
+    ['Alandur','Ambattur','Egmore','Guindy','Mambalam','Purasavakkam','Perambur','Tondiarpet'],
+    ['Annur','Anamalai','Mettupalayam','Irugur','Pollachi','Peelamedu','Karumattampatti','Singanallur','Somanur','Sulur'],
+    ['Erode','Perundurai','Chennimalai','Siruvaliur','Kavundapadi','Sittodu','Kunnattur','Gobichettipalyam','Attani','Kurumandur','Bhavani','Andiyur','Nambiyur','Punjai Puliyampatti'],
+    ['Selam','Omalur','Yercaud','Mallur'],
+    ['Tiruppur','Perumanallur','Avanashi','Mangalam','Sevur','Kurumandur','Uttukkuli','Dharapuram','Kangayam','Padiyur','Vellakkovil','Muttur','Udumalaippettai','Madattukkulam','Gudimangalam'],
+    ['Ooty','Coonoor','Kotagiri','Wellington Town','Aravankadu']
+    ]
+    document.getElementById('area').remove();
+    var select = document.createElement("select");
+    select.setAttribute('class','form-select border-0 text-white');
+    select.setAttribute('id','area');
+    select.setAttribute('style','background-color: rgba(185, 182, 182, 0.294);');
+    document.getElementById('set').appendChild(select);
+
+    var option1 = document.createElement("option");
+        option1.setAttribute('id',"opt01")
+        option1.setAttribute("selected","");
+        document.getElementById("area").appendChild(option1);
+        document.getElementById("opt01").innerHTML = "Select Area";
+
+    for (i=0;i < areas[input].length; i++){
+        var option = document.createElement("option");
+        option.setAttribute('id',"opt"+i)
+        option.setAttribute('value',areas[input][i]);
+        document.getElementById("area").appendChild(option);
+        document.getElementById("opt"+i).innerHTML = areas[input][i];
+        console.log(i)
+
+       
+    }
+    
+
+}
+
+
 function weather(){
-    var place =  document.getElementById("place").value
+    var place =  document.getElementById("area").value
+    console.log(place)
     link='https://api.weatherapi.com/v1/current.json?key=7193a037355848eca6921203212806&q='+place
     
     async function result(){
